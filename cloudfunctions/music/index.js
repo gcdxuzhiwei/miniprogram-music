4dee683473a16ec5b797854bf44087d6de88dd06
xuzhiwei <xuzhiwei4347@qq.com>
2020-06-27 23:49:04 +0800

    获取歌曲列表

--- cloudfunctions/music/index.js
+++ cloudfunctions/music/index.js
@@ -1,6 +1,8 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
 const TcbRouter=require('tcb-router')
+const rp=require('request-promise')
+const BASE_URL='http://musicapi.xiecheng.live'
 cloud.init()
 
 // 云函数入口函数
@@ -14,6 +16,12 @@ exports.main = async (event, context) => {
     .get()
     .then((res)=>{
       return res
+    })
+  })
+  app.router('musiclist',async(ctx,next)=>{
+    ctx.body=await rp(BASE_URL+'/playlist/detail?id='+parseInt(event.playlistId))
+    .then((res)=>{
+      return JSON.parse(res)
     })
   })
   return app.serve()
