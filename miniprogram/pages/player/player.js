f92bc953fc560503b2bb820d4a17ec07497bb3b2
xuzhiwei <xuzhiwei4347@qq.com>
2020-07-01 10:49:23 +0800

    播放页布局

--- miniprogram/pages/player/player.js
+++ miniprogram/pages/player/player.js
@@ -1,13 +1,15 @@
 // pages/player/player.js
 let musiclist=[]
 let nowPlayingIndex=0
+const backgroundAudioManager=wx.getBackgroundAudioManager()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    picUrl:''
+    picUrl:'',
+    isPlaying:false
   },
 
   /**
@@ -16,15 +18,47 @@ Page({
   onLoad: function (options) {
     nowPlayingIndex=options.index
     musiclist=wx.getStorageSync('musiclist')
-    this._loadMusicDetail()
+    this._loadMusicDetail(options.musicId)
   },
-  _loadMusicDetail(){
+  _loadMusicDetail(musicId){
     let music=musiclist[nowPlayingIndex]
     wx.setNavigationBarTitle({
       title: music.name,
     })
     this.setData({
-      picUrl:music.al.picUrl
+      picUrl:music.al.picUrl,
+      isPlaying:false
+    })
+    wx.showLoading({
+      title: '加载中',
+    })
+    wx.cloud.callFunction({
+      name:'music',
+      data:{
+        $url:'musicUrl',
+        musicId:musicId
+      }
+    }).then((res)=>{
+      let result=JSON.parse(res.result)
+      backgroundAudioManager.src=result.data[0].url
+      backgroundAudioManager.title=music.name
+      backgroundAudioManager.coverImgUrl=music.al.picUrl
+      backgroundAudioManager.singer=music.ar[0].name
+      backgroundAudioManager.epname=music.al.name
+      this.setData({
+        isPlaying:true
+      })
+      wx.hideLoading()
+    })
+  },
+  togglePlaying(){
+    if(this.data.isPlaying){
+      backgroundAudioManager.pause()
+    }else{
+      backgroundAudioManager.play()
+    }
+    this.setData({
+      isPlaying:!this.data.isPlaying
     })
   },
   /**
